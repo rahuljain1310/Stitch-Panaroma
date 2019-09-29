2ef84d4a204b0c9f79a677f41cb5d5e002acb9c4
rahuljain1310 <ee1170476@iitd.ac.in>
2019-09-29 21:26:35 +0530

    Loop Created

--- Stitching.py
+++ Stitching.py
@@ -4,9 +4,14 @@ import matplotlib.pyplot as plt
 from random import randrange
 import os
 
-def getMatches(desList, i, j):
-  matches = bf.knnMatch(desList[i], desList[j], k=2)
-  # Apply ratio test
+indicexMax = lambda x: np.unravel_index(np.argmax(x),x.shape)
+
+def showTestImage(image):
+  cv2.imshow('test',image)
+  cv2.waitKey(60)
+
+def getMatches(desListi, desListj):
+  matches = bf.knnMatch(desListi, desListj, k=2)
   good = []
   for m in matches:
     if m[0].distance < 0.5*m[1].distance:
@@ -14,46 +19,72 @@ def getMatches(desList, i, j):
   matches = np.asarray(good)
   return len(matches), matches
 
+def mergeImage(img1,img2,ds1,ds2,kp1,kp2):
+  pass
+
 if __name__ == "__main__":
-  ## Read all Images in An Array
-  print("Reading all Images from the Directory /1")
+  ## Settings
   directory = './1'
   imagelist = [f for f in os.listdir(directory) if f.endswith('.jpg')]
   N_Images = len(imagelist)
   ScaleFactor = 8
-  imgListCV = []
-  for image in imagelist:
+
+  ## Initializing Lists and Functions
+  imgListCV, kpList, desList = [],[],[]
+  sift = cv2.xfeatures2d.SIFT_create()
+  bf = cv2.BFMatcher()
+
+  ## Read all Images in An Array
+  ## find the keypoints and descriptors with SIFT and Add to List
+  print("Reading all Images from the Directory {0}".format(directory))
+
+  for image in imagelist:  
     img = cv2.imread(directory+'/'+image)
     height,width,_ = img.shape
     img = cv2.resize(img,(int(width/ScaleFactor),int(height/ScaleFactor)))
+    kp, des = sift.detectAndCompute(img,None)
+    kpList.append(kp)
+    desList.append(des)
     imgListCV.append(img)
     # cv2.imshow('Image',img)
     # cv2.waitKey(30)
 
-  # find the keypoints and descriptors with SIFT
-  print("Find all keypoints and descriptors with SIFT")
-  sift = cv2.xfeatures2d.SIFT_create()
-  kpList, desList = [],[]
-  for image in imgListCV:
-    kp, des = sift.detectAndCompute(image,None)
+  while len(imgListCV)>1:
+    N = len(imgListCV)
+    matchMatrix = np.zeros((N,N))
+    for i in range(0,N):
+      print("Matching with Image {0}".format(i))
+      for j in range(0,N):
+        if i==j:
+          continue
+        matchCount, matches = getMatches(desList[i],desList[j])
+        print("Match Count {0} : {1}".format(j ,matchCount))
+        matchMatrix[i][j] = matchCount
+    print(matchMatrix)
+    i,j = indicexMax(matchMatrix)
+    if j<i:
+      i,j = j,i
+    assert i<j
+
+    ## Popping Chosen Images 
+    img1 = imgListCV.pop(i)
+    showTestImage(img1)
+    img2 = imgListCV.pop(j-i-1)
+    showTestImage(img2)
+    ds1 = desList.pop(i)
+    ds2 = desList.pop(j-i-1)
+    kp1 = kpList.pop(i)
+    kp2 = kpList.pop(j-i-1)
+    assert len(imgListCV) == len(kpList) == len(desList) == N-2
+
+    imgMerged = mergeImage(img1,img2,ds1,ds2,kp1,kp2)
+    kp, des = sift.detectAndCompute(imgMerged,None)
+    imgListCV.append(img)
     kpList.append(kp)
     desList.append(des)
-
-  ## Arranging all images
-  print("Estimating Order of Images by Count Match Matrix.")
-  bf = cv2.BFMatcher()
-  matchMatrix = np.zeros((N_Images,N_Images))
-  for i in range(0,N_Images):
-    print("Matching with Image {0}".format(i))
-    for j in range(0,N_Images):
-      if i==j:
-        continue
-      matchCount, matches = getMatches(desList,i,j)
-      print("Match Count {0} : {1}".format(j ,matchCount))
-      matchMatrix[i][j] = matchCount
+    assert len(imgListCV) == len(kpList) == len(desList) == N-1
     
-  print(matchMatrix)
-      
+        
 
 # img_ = cv2.imread('right.jpeg')
 # img1 = cv2.cvtColor(img_, cv2.COLOR_BGR2GRAY)
