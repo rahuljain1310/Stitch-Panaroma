7dddd39eaf36443b7f8ebe27ffc73918bb765591
Vishwajeet Agrawal <vishwa.grawal@gmail.com>
2019-09-30 20:28:55 +0530

    ad

--- Panaroma.py
+++ Panaroma.py
@@ -36,9 +36,24 @@ def find_metric(point,poly,dists):
     pt1 = Point(poly_coords[0])
     dis1 = p.distance(pt1)
     for point,dist in zip(poly_coords[1:],dists):
-        dis2 = p.distance(point)
-        metrics.append(abs(dis2+dis1-dist))
+        dis2 = p.distance(Point(point))
+        metrics.append(abs((dis2+dis1-dist)**(0.5)))
         dis1 = dis2
+    return min(metrics)
+
+def find_metric2(point,poly,dists):
+    p = Point(point)
+    poly_coords = poly.exterior.coords
+
+    # points = 
+    metrics = []
+    pt1 = Point(poly_coords[0])
+    # dis1 = p.distance(pt1)
+    for point,dist in zip(poly_coords[1:],dists):
+        line1 = LineString([pt1,Point(point)])
+        dis2 = p.distance(line1)
+        metrics.append(dis2)
+        pt1 = Point(point)
     return min(metrics)
 
 def calculate_dists(poly):
@@ -46,7 +61,7 @@ def calculate_dists(poly):
     dists = []
     pt1 = Point(poly_coords[0])
     for point in poly_coords[1:]:
-        dists.append(pt1.distance(point))
+        dists.append(pt1.distance(Point(point)))
         pt1 = Point(point)
     return dists
 
@@ -55,7 +70,7 @@ def calculate_image_metric(w,h,poly):
     image_metrics = np.ndarray([h,w],dtype=np.float32)
     for i in range(w):
         for j in range(h):
-            image_metrics[j][i] = find_metric(Point(i,j),poly,dists)
+            image_metrics[j][i] = find_metric2(Point(i,j),poly,dists)
     return image_metrics
 
 def get_mask(w,h,quad):
@@ -79,9 +94,11 @@ def get_quad(width,height,H):
 
 def get_new_mask(mask1,tx,ty,h,w):
   mask1_new = np.ndarray((h,w),dtype=np.bool)
+  # print(int(tx),int(ty))
+  
   for i in range(h):
     for j in range(w):
-      if (i>=ty and j >=tx):
+      if (i>=ty and j >=tx and i - ty< mask1.shape[0] and j -tx< mask1.shape[1]):
         mask1_new[i][j] = mask1[i-ty][j-tx]
       else:
         mask1_new[i][j] = 0
@@ -94,13 +111,18 @@ def shift_poly(poly,tx,ty):
   return Polygon(newpoints)
 
 def blend(img1,mask1,metric1,img2,mask2,metric2):
-  dst = np.array(img1)
+
   assert(img1.shape == img2.shape)
-  for i in img1.shape[0]:
-    for j in img1.shape[1]:
+  plt.imshow(mask1)
+  plt.show()
+  plt.imshow(mask2)
+  plt.show()
+  dst = np.ndarray(img1.shape,dtype = np.uint8)
+  for i in range(img1.shape[0]):
+    for j in range(img1.shape[1]):
       if (mask1[i][j] and mask2[i][j]):
         for k in range(3):
-          dst[i][j][k] = np.uint8(min((metric1[i][j]*img1[i][j][k] + metric2[i][j]*img2[i][j][k])/(metric1[i][j] + metric2[i][j]),255))
+          dst[i][j][k] = np.uint8(min((metric1[i][j]*np.float32(img1[i][j][k]) + metric2[i][j]*np.float32(img2[i][j][k]))/(metric1[i][j] + metric2[i][j]),255))
         # dst[i][j] = np.array((metric1[i][j]*img1[i][j] + metric2[i][j]*img2[i][j])/(metric1[i][j] + metric2[i][j]),
       elif (mask1[i][j]):
         dst[i][j] = img1[i][j].copy()
@@ -108,35 +130,44 @@ def blend(img1,mask1,metric1,img2,mask2,metric2):
         dst[i][j] = img2[i][j].copy()
       else:
         dst[i][j] = np.array([0,0,0],dtype=np.uint8)
+  print(dst.shape)
   return dst
 
 def combine(img1,H1,img2,H2,w,h,mask1,poly):
+    print(w,h)
     img_1 = cv2.warpPerspective(img1,H1,(w,h))
-    img_2 = cv2.warpPersepctive(img2,H2,(w,h))
-    tx = H1[0][2]
-    ty = H1[1][2]
+    plt.imshow(img_1)
+    plt.show()
+    
+    img_2 = cv2.warpPerspective(img2,H2,(w,h))
+    plt.imshow(img_2)
+    plt.show()
+    print(img_1.shape,img_2.shape)
+    tx = int(H1[0][2])
+    ty = int(H1[1][2])
+    print(tx,ty)
     mask1_new = get_new_mask(mask1,tx,ty,h,w)
     quad = get_quad(img2.shape[1],img2.shape[0],H2)
     mask2 = get_mask(w,h,quad)
     mask_new = mask1_new + mask2
-    poly1_new = shift_poly(poly)
+    poly1_new = shift_poly(poly,tx,ty)
     poly_new = poly1_new.union(quad)
     img1_metric = calculate_image_metric(w,h,poly1_new)
     img2_metric = calculate_image_metric(w,h,quad)
-    dst = blend(img1,mask1_new,img1_metric,img2,mask2,img2_metric)
+    dst = blend(img_1,mask1_new,img1_metric,img_2,mask2,img2_metric)
     return (dst,mask_new,poly_new)
   
 def panaroma(imglist,Hpairs,wh):
     img1 = imglist[0]
     mask1 = np.ones([img1.shape[0],img1.shape[1]],dtype=np.bool)
-    poly1 = Polygon((0,0),(img1.shape[1],0),(img1.shape[1],img1.shape[0]),(0,img1.shape[0]))
+    poly1 = Polygon([(0,0),(img1.shape[1],0),(img1.shape[1],img1.shape[0]),(0,img1.shape[0])])
 
-    for i in range(Hpairs):
+    for i in range(len(Hpairs)):
       H1,H2 = Hpairs[i]
       w,h = wh[i]
       img2 = imglist[i+1]
       img1,mask1,poly1 = combine(img1,H1,img2,H2,w,h,mask1,poly1)
-    
+    print(img1.shape)
     return img1
     
 # def get_mask(img,corners):
@@ -256,7 +287,7 @@ def Laplacian_Pyramid_Blending_with_mask(A, B, m1,m2, num_levels = 6):
 #     cv2.imwrite("lpb.png",lpb)
 
 img_ = cv2.imread('right.jpeg')
-ScaleFactor = 2
+ScaleFactor = 4
 height,width,_ = img_.shape
 img_ = cv2.resize(img_,(int(width/ScaleFactor),int(height/ScaleFactor)))
 
@@ -310,21 +341,31 @@ else:
   raise AssertionError("Can't find enough keypoints.")
 # dst = cv2.warpAffine(img_,H,(img.shape[1] + img_.shape[1], img.shape[0]))
 # dst = cv2.warpPerspective(img_,H,(img.shape[1] + img_.shape[1], img.shape[0]+img_.shape[0]))
-dst = cv2.warpPerspective(img_,H,(1024,1024))
-# plt.subplot(122),plt.imshow(dst),plt.title('Warped Image')
-# plt.show()
-# plt.figure()
-img_t1 = np.zeros_like(dst, dtype=np.uint8)
-mask1 = np.zeros([dst.shape[0],dst.shape[1]],dtype=np.uint8)
-img_t1[0:img.shape[0],0:img.shape[1]] = img
-mask1[0:img.shape[0],0:img.shape[1]] = 1
-print(mask1.shape, dst.shape, img_t1.shape)
-dst1,mkdst = laplacian_blending(img_t1,dst,mask1,H,img2.shape[1],img2.shape[0])
+# dst = cv2.warpPerspective(img_,H,(1024,1024))
+# # plt.subplot(122),plt.imshow(dst),plt.title('Warped Image')
+# # plt.show()
+# # plt.figure()
+# img_t1 = np.zeros_like(dst, dtype=np.uint8)
+# mask1 = np.zeros([dst.shape[0],dst.shape[1]],dtype=np.uint8)
+# img_t1[0:img.shape[0],0:img.shape[1]] = img
+# mask1[0:img.shape[0],0:img.shape[1]] = 1
+# print(mask1.shape, dst.shape, img_t1.shape)
+# dst1,mkdst = laplacian_blending(img_t1,dst,mask1,H,img2.shape[1],img2.shape[0])
+img1 = img
+img2 = img_
+H1 = np.array([[1,0,0],[0,1,0],[0,0,1]],dtype=np.float32)
+H2 = H
+w = img1.shape[1] + img2.shape[1]
+h = img1.shape[0] + img2.shape[0]
+imgs = [img1,img2]
+wh = [(w,h)]
+HS = [(H1,H2)]
 
+dst1 = panaroma(imgs,HS,wh)
 
 # dst[0:img.shape[0], 0:img.shape[1]] = img
 cv2.imwrite('output.jpg',dst1)
-plt.imshow(dst)
+plt.imshow(dst1)
 plt.show()
 
 print(matches.shape)
